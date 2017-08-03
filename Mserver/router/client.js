6f0ddffe3a5bf43552f5938c66b304a1281d193b
wang839305939 <wang839305939@outlook.com>
2017-08-03 19:26:08 +0800

    封装文件的读写方法

--- Mserver/router/client.js
+++ Mserver/router/client.js
@@ -1,11 +1,12 @@
-let fs = require("fs");
 let request = require("request");
 let config = require("../config/config");
+let All = require("../fileOperate/clientInfo");
+let Offline = require("../fileOperate/offline")
 module.exports = {
 		login: function(req, res) {
 			console.log("+++++收q到客户端注册请求++++++");
 			let ip = req.hostname;
-			fs.readFile("./data/clientInfo.txt", function(err, data) {
+			All.getAllNum(function(err, data) {
 				if(err) {
 					throw err;
 				}
@@ -19,7 +20,7 @@ module.exports = {
 						}
 					})
 				} else {
-					fs.appendFile("./data/clientInfo.txt", ip + ";", function(err) {
+					All.addClient( ip + ";", function(err) {
 						if(err) {
 							res.send({
 								result: {
@@ -42,23 +43,33 @@ module.exports = {
 		},
 		keepAlive:function(callback) {
 			setInterval(function(){
-				fs.readFile("./data/clientInfo.txt", function(err,data) {
+				All.getAllNum(function(err,data) {
 					var clients = data.toString().split(";");
 					clients.forEach(function(item, index) {
-						if(item){
+						if(item){				
 							request.get("http://"+item +":"+config.base.clientPort+"/client/isOnline", function(err, res, body) {
 								if(err) {
-									console.log(item + "离线");
+									//修改在线文件
 									clients = clients.map(function(ip,index){
-										return ip!=item;
+										console.log(ip+"--:"+item)
+										if(ip&ip!=undefined&ip!=item){
+											return item;
+										}		
 									});
-									fs.writeFile("./data/clientInfo.txt",clients.join(";"),function(err){
+									All.init(clients.join(";"),function(err){
 										if(err){
 											console.log("文件修改发生错误");
 										}
 									});
+									//修改离线文件
+									Offline.addClient(item+";",function(err){
+										if(err){
+											console.log("离线文件添加失败")
+										    return;
+										}
+										    console.log(item + "离线");
+									});									
 								} else {
-									console.log(res)
 									console.log(item + "在线")
 								}
 						   })
--- Mserver/server.js
+++ Mserver/server.js
@@ -1,17 +1,23 @@
 let config = require(__dirname + "/config/config");
 let express = require("express");
 let app = express();
-let fs = require("fs");
+let All = require("./fileOperate/clientInfo");
+let Offline = require("./fileOperate/offline")
 let client = require("./router/client");
 app.use(express.static("./public"));
+
 console.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>核心服务开始启动<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 //接受客户端服务注册请求
 app.post("/client/login", client.login)
 app.listen(config.base.port);
 console.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>核心服务启动成功<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 //清除缓存在线设备文件
-fs.writeFile("./data/clientInfo.txt", "", function(err) {
-	client.keepAlive(function() {
+All.init( "", function(err) {
+});
+//清除缓存离线设备文件
+Offline.init( "", function(err) {
+})
+//和客户端保持心跳
+client.keepAlive(function() {
 		console.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>启动心跳保持成功<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-	})
 })
